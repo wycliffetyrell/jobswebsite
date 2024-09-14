4a2cdefa496c4e21982b64f36b2210bcf53200b1
wycliffetyrell <wycliffebundi3@gmail.com>
2024-09-14 08:58:08 +0300

    made components such as layout, tags and job cards

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+ <div class="space-y-10">
+    <section>
+        <x-section-heading>Top Jobs</x-section-heading>
+        <div class="grid lg:grid-cols-3 gap-8 mt-6">
+            <x-job-card />
+            <x-job-card />
+            <x-job-card />
+        </div>
+    </section>
+
+    <section>
+        <x-section-heading>Tags</x-section-heading>
+        <div class="mt-6 space-x-1">
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+        </div>
+
+    </section>
+    <section>
+        <x-section-heading>Recent Jobs</x-section-heading>
+
+
+    </section>
+ </div>
+</x-layout>
